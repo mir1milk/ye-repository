c879b8d37d020e6bc8a635c4435558923a928ea6
miri <mir1milk@gmail.com>
2024-08-26 22:58:07 +0900

    good

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Wrapper from '@/components/Wrapper.tsx';
 import Account from '@/layout/Account/Account.tsx';
 import Container from '@/layout/Container.tsx';
 import FloatingBar from '@/layout/FloatingBar/FloatingBar.tsx';
-import GalleryWrap from '@/layout/Gallery/GalleryWrap.tsx';
+// import GalleryWrap from '@/layout/Gallery/GalleryWrap.tsx';
 import Guestbook from '@/layout/Guestbook/Guestbook.tsx';
 import Invitation from '@/layout/Invitation/Invitation.tsx';
-import Location from '@/layout/Location/Location.tsx';
+// import Location from '@/layout/Location/Location.tsx';
 import Main from '@/layout/Main/Main.tsx';
 
 function App() {
-  const ncpClientId = import.meta.env.VITE_APP_NAVERMAPS_CLIENT_ID;
+  const ncpClientId = import.meta.env.REACT_APP_NAVERMAPS_CLIENT_ID;
   const [isVisible, setIsVisible] = useState(false);
   const galleryRef = useRef(null);
 
@@ -48,7 +48,7 @@ function App() {
         </Wrapper>
         <Wrapper ref={galleryRef}>
           <Heading1>Gallery</Heading1>
-          <GalleryWrap />
+          {/* <GalleryWrap /> */}
         </Wrapper>
         <Wrapper>
           <Heading1>마음 전하실 곳</Heading1>
@@ -56,7 +56,7 @@ function App() {
         </Wrapper>
         <Wrapper>
           <Heading1>오시는 길</Heading1>
-          <Location />
+          {/* <Location /> */}
         </Wrapper>
         <Wrapper>
           <Heading1>신랑 신부에게</Heading1>
