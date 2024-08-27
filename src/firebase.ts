1abab50b1803b8955c4e9e5bf2a7324d1d9c5ed5
mir1milk <mir1milk@gmail.com>
2024-08-27 19:21:21 +0900

    commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,24 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// TODO: https://firebase.google.com/docs/web/setup?hl=ko
+// 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-  apiKey: "AIzaSyCPxnvv7kIlgvFg7-RMROcuCTUFIOm8yVw",
-  authDomain: "gest-9bc1e.firebaseapp.com",
-  projectId: "gest-9bc1e",
-  storageBucket: "gest-9bc1e.appspot.com",
-  messagingSenderId: "853741126072",
-  appId: "1:853741126072:web:f9acc0ea632d2b61ebcf4d",
-  measurementId: "G-D80J66LJP4"
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const realtimeDb = getDatabase(app);
-
+export const firebaseApp = initializeApp(firebaseConfig);
+export const realtimeDb = getDatabase(firebaseApp);
