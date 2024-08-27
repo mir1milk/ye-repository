0f643280360fc7e970c3729ab90e9ed78838afe0
mir1milk <mir1milk@gmail.com>
2024-08-27 14:14:17 +0900

    commit error

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/compat/app';
 import { getDatabase } from 'firebase/database';
 
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
